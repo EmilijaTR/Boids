5810c79c5d7e874e322d5cd2c97eed9d577ddb46
EmilijaTR <emilijat1234@gmail.com>
2025-01-03 23:17:32 +0100

    Drawing one boid in the center of the screen.

---
+++ Boids/src/main/java/org/example/Boids.java
@@ -0,0 +1,41 @@
+package org.example;
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Boids extends JPanel {
+    private ArrayList<Boid> flock = new ArrayList<>();
+
+    public Boids() {
+        setPreferredSize(new Dimension(640, 360));
+        flock.add(new Boid(640, 360));
+
+        Timer timer = new Timer(16, e -> {
+            repaint();
+        });
+        timer.start();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, getWidth(), getHeight());
+
+        g.setColor(Color.WHITE);
+        for (Boid boid : flock) {
+            boid.show(g);
+        }
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Boids");
+        Boids simulation = new Boids();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(simulation);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
