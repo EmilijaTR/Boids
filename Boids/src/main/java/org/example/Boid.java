0763412725417642075c21c57760539d2a0d348e
EmilijaTR <emilijat1234@gmail.com>
2025-01-15 00:28:35 +0100

    Sliders added.

--- Boids/src/main/java/org/example/Boid.java
+++ Boids/src/main/java/org/example/Boid.java
@@ -1,22 +1,131 @@
 package org.example;
 
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 public class Boid {
     private Vector2D position;
     private Vector2D velocity;
     private Vector2D acceleration;
 
+    private double maxForce = 0.2;
+    private double maxSpeed = 5;
+
     public Boid(double width, double height) {
-        this.position = new Vector2D(width / 2, height / 2);
+        this.position = new Vector2D(Math.random() * width, Math.random() * height);
         this.velocity = Vector2D.random2D();
+        this.velocity.multiply(2 + Math.random() *2);
         this.acceleration = new Vector2D(0, 0);
     }
+
+    public void edges(double width, double height) {
+        if(position.x > width) position.x = 0;
+        else if(position.x < 0) position.x = width;
+
+        if(position.y > height) position.y = 0;
+        else if(position.y < 0) position.y = height;
+    }
+
+    public Vector2D alignment(ArrayList<Boid> boids) {
+        double perceptionRadius = 25;
+        Vector2D steering = new Vector2D(0, 0);
+        int total = 0;
+
+        for (Boid other : boids) {
+            double d = position.distance(other.position);
+            if (other != this && d < perceptionRadius) {
+                steering.add(other.velocity);
+                total++;
+            }
+        }
+
+        if (total > 0) {
+            steering.divide(total); //to avg it
+            steering.normalize();   //to only consider direction, making it unit vector
+            steering.multiply(maxSpeed);    //give appropriate speed(mag)
+            steering.subtract(velocity);    //direction in order to align
+            steering.limit(maxForce);       //limit so it doesn't change all of a sudden
+        }
+
+        return steering;
+    }
+
+    public Vector2D cohesion(ArrayList<Boid> boids) {
+        double perceptionRadius = 75;
+        Vector2D steering = new Vector2D(0, 0);
+        int total = 0;
+
+        for (Boid other : boids) {
+            double d = position.distance(other.position);
+            if (other != this && d < perceptionRadius) {
+                steering.add(other.position);
+                total++;
+            }
+        }
+
+        if (total > 0) {
+            steering.divide(total);
+            steering.subtract(position);
+            steering.normalize();
+            steering.multiply(maxSpeed);
+            steering.subtract(velocity);
+            steering.limit(maxForce);
+        }
+        return steering;
+    }
+    public Vector2D separation(ArrayList<Boid> boids) {
+        double perceptionRadius = 50;
+        Vector2D steering = new Vector2D(0, 0);
+        int total = 0;
+
+        for (Boid other : boids) {
+            double d = position.distance(other.position);
+            if (other != this && d < perceptionRadius) {
+                Vector2D diff = Vector2D.subtract(position, other.position);    //direction in oposite
+                diff.divide(d*d);   //closer the boid, stronger force to move
+                steering.add(diff);
+                total++;
+            }
+        }
+
+        if (total > 0) {
+            steering.divide(total);
+            steering.normalize();
+            steering.multiply(maxSpeed);
+            steering.subtract(velocity);
+            steering.limit(maxForce);
+        }
+        return steering;
+    }
+
+
+
+    public void flock(ArrayList<Boid> boids, double cohesionWeight, double alignmentWeight, double separationWeight) {
+        acceleration = new Vector2D(0, 0);
+
+        Vector2D alignment = alignment(boids);
+        Vector2D cohesion = cohesion(boids);
+        Vector2D separation = separation(boids);
+
+        alignment.multiply(alignmentWeight);
+        cohesion.multiply(cohesionWeight);
+        separation.multiply(separationWeight);
+
+        acceleration.add(alignment);
+        acceleration.add(cohesion);
+        acceleration.add(separation);
+    }
+
+
+
     public void update() {
         position.add(velocity); //moves in the direction of the velocity
         velocity.add(acceleration); //changes the velocity based on the current acceleration
+        acceleration.multiply(0);//it is diff for every frame
     }
+
+
     public void show(Graphics g) {
-        g.fillOval((int) position.x - 8, (int) position.y - 8, 16, 16); // Draw as a small circle
+        g.fillOval((int) position.x - 4, (int) position.y - 4, 8, 8);
     }
 }
--- Boids/src/main/java/org/example/Boids.java
+++ Boids/src/main/java/org/example/Boids.java
@@ -1,23 +1,62 @@
 package org.example;
+
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
 public class Boids extends JPanel {
     private ArrayList<Boid> flock = new ArrayList<>();
+    private JSlider cohesionSlider;
+    private JSlider alignmentSlider;
+    private JSlider separationSlider;
+
+    private double cohesionWeight = 1.0;
+    private double alignmentWeight = 1.0;
+    private double separationWeight = 1.0;
 
     public Boids() {
-        setPreferredSize(new Dimension(640, 360));
+        setPreferredSize(new Dimension(640, 400));
+        setLayout(new BorderLayout());
+
         for (int i = 0; i < 100; i++) {
             flock.add(new Boid(640, 360));
-
         }
 
-        Timer timer = new Timer(16, e -> {
+        JPanel sliderPanel = new JPanel();
+        sliderPanel.setLayout(new GridLayout(2, 3));
+        sliderPanel.setOpaque(false);
+
+
+        JLabel cohesionLabel = new JLabel("Cohesion", JLabel.CENTER);
+        JLabel alignmentLabel = new JLabel("Alignment", JLabel.CENTER);
+        JLabel separationLabel = new JLabel("Separation", JLabel.CENTER);
+
+        cohesionSlider = new JSlider(JSlider.HORIZONTAL, 0, 100, 50);
+        alignmentSlider = new JSlider(JSlider.HORIZONTAL, 0, 100, 50);
+        separationSlider = new JSlider(JSlider.HORIZONTAL, 0, 100, 50);
+        cohesionSlider.setBackground(new Color(0, 0, 0, 0));
+        alignmentSlider.setBackground(new Color(0, 0, 0, 0));
+        separationSlider.setBackground(new Color(0, 0, 0, 0));
+
+        sliderPanel.add(cohesionLabel);
+        sliderPanel.add(alignmentLabel);
+        sliderPanel.add(separationLabel);
+
+        sliderPanel.add(cohesionSlider);
+        sliderPanel.add(alignmentSlider);
+        sliderPanel.add(separationSlider);
+
+        add(sliderPanel, BorderLayout.SOUTH);
+
+        Timer timer = new Timer(16, e -> {  //60fps
+            cohesionWeight = cohesionSlider.getValue() / 50.0; // Scale slider values
+            alignmentWeight = alignmentSlider.getValue() / 50.0;
+            separationWeight = separationSlider.getValue() / 50.0;
+
             for (Boid boid : flock) {
                 boid.edges(640, 360);
-                boid.flock(flock);
-                boid.update(); //update the position of the boid
+                boid.flock(flock, cohesionWeight, alignmentWeight, separationWeight);
+                boid.update();
             }
             repaint();
         });
